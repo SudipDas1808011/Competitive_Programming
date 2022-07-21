7410be7d146d0f77a07451cd95ff1f6ccb81fda5
sudip das <sudipdas11804144@gmail.com>
2022-07-21 18:23:07 +0600

    codechef

---
+++ Codechef/FAIRPASS.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void solve(){
+    int k,n;
+    cin>>n>>k;
+    if(k>n)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    
+}
+int main() {
+	int tc;
+	cin>>tc;
+	while(tc--){
+	    solve();
+	}
+	return 0;
+}
